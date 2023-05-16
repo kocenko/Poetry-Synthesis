21fa1102d1212d60c52896db5a5be44d117bc8e3
Gabriel Kocenko <gabryelkocenko@gmail.com>
2023-05-16 17:06:11 +0200

    Changed max iterations to epochs

--- train.py
+++ train.py
@@ -3,7 +3,7 @@ from dataloader import custom_dataset
 import tiktoken
 
 
-max_iterations = 100  # 6000 would be better
+epochs = 1  # Just for now
 split_ratio = 0.85
 context_length = 8
 offset = 1  # I am wondering what would be the results for 2, for example
@@ -13,8 +13,4 @@ tokenizer = tiktoken.get_encoding("cl100k_base")
 vocab_size = tokenizer.n_vocab
 
 train_set = custom_dataset.PoeDataset(file_path, 'train', split_ratio, context_length, tokenizer, offset=offset)
-train_dataloader = DataLoader(train_set, batch_size=batch_size, shuffle=True)
-train_iterator = iter(train_dataloader)
-
-for _ in range(10):
-    print(next(train_iterator))
+train_dataloader = DataLoader(train_set, batch_size=batch_size, shuffle=True, drop_last=True)
